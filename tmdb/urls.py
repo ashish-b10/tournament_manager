393743abf759bfea71ac3c8666b119ae9eafc02b
Ashish Banerjee <profcalc14@yahoo.com>
2015-10-10 08:22:02 -0400

    Shuffle around the user interface
    Move the list of matches from index.html to division.html
    
    index.html will display a list of divisions and rings
    
    Create rings.html which displays all matches in a ring

--- tmdb/urls.py
+++ tmdb/urls.py
@@ -3,7 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    #url(r'^$', views.index, name='team'),
-    url(r'^match/(?P<match_num>[0-9]+)/$', views.match, name='match'),
+    url(r'^match/(?P<match_num>[0-9]+)/*$', views.match, name='match'),
+    url(r'^rings/*$', views.rings, name='rings'),
+    url(r'^division/(?P<division_id>[0-9]+)/*$', views.division,
+            name='division'),
+    url(r'^division/*$', views.division, name='division'),
     url(r'^$', views.index, name='index'),
 ]
--- tmdb/views.py
+++ tmdb/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
-from .models import TeamMatch, Team
+from .models import TeamMatch, Team, Division
 from .forms import MatchForm
 
+from collections import defaultdict
+
 def index(request):
-    team_matches = TeamMatch.objects.all().order_by('number')
-    context = {'team_matches' : team_matches}
+    divisions = Division.objects.all()
+    context = {'divisions' : divisions}
     return render(request, 'tmdb/index.html', context)
+
+def division(request, division_id=None):
+    if division_id is None:
+        divisions = Division.objects.all()
+    else:
+        divisions=[Division.objects.get(pk=division_id)]
+
+    matches = []
+    for division in divisions:
+        team_matches = TeamMatch.objects.filter(
+                division=division).order_by('number')
+        matches.append((division, team_matches))
+    context = {'team_matches' : matches}
+    return render(request, 'tmdb/division.html', context)
 
 def match(request, match_num):
     match = TeamMatch.objects.get(number=match_num)
@@ -24,4 +40,17 @@ def match(request, match_num):
 
     return render(request, 'tmdb/match_edit.html', {'form': form,
             'match_num': match_num})
-    #return HttpResponse('Editing match number ' + match_num)
+
+def rings(request):
+    if 'all_matches' not in request.GET: all_matches=False
+    else:
+        try:
+            all_matches = int(request.GET['all_matches']) > 0
+        except ValueError:
+            all_matches = False
+    matches_by_ring = defaultdict(list)
+    for match in TeamMatch.objects.filter(ring_number__isnull=False):
+        if all_matches or match.winning_team is None:
+            matches_by_ring[str(match.ring_number)].append(match)
+    context = {'matches_by_ring' : sorted(matches_by_ring.items())}
+    return render(request, 'tmdb/rings.html', context)
