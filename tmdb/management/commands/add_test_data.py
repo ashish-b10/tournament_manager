addcf0a068cd6b52c5709499fb97269e5684a8f1
Ashish Banerjee <profcalc14@yahoo.com>
2015-12-19 09:19:13 -0500

    Create sample tournament data

--- tmdb/management/commands/add_test_data.py
+++ tmdb/management/commands/add_test_data.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python3
 from django.core.management.base import BaseCommand
 import tmdb.models as mdls
+from datetime import date
 
 class LoadSampleTmdbData():
     def __init__(self):
         self.num_teams = 5
 
         self.add_global_data()
+        self.add_tournaments()
         self.add_orgs()
         self.add_teams()
         self.add_matches(self.divisions[0])
+
+    def add_tournaments(self):
+        # registration_doc_urls are just nonsense placefillers
+        self.mit2015 = mdls.Tournament(date=date(year=2015, month=10, day=11),
+                location="MIT",
+                registration_doc_url="http://ectc-online.org/mit-2015")
+        self.mit2015.save()
+        self.cornell2015 = mdls.Tournament(date=date(year=2015, month=11,
+                day=1), location="Cornell",
+                registration_doc_url="http://ectc-online.org/cornell-2015")
+        self.cornell2015.save()
 
     def add_orgs(self):
         self.orgs = {}
