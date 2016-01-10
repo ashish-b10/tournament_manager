035ec482217c749eba31a1b183d43a7d35eb37b8
Ashish Banerjee <profcalc14@yahoo.com>
2016-01-10 11:43:15 -0500

    Remove some dead code
    tournament_edit is now only used for editing existing tournaments, not
    new ones which have not been created yet.

--- tmdb/views.py
+++ tmdb/views.py
@@ -40,13 +40,9 @@ def tournament_edit(request, tournament_slug):
             tournament = edit_form.save()
             return HttpResponseRedirect(reverse('tmdb:index'))
     else:
-        if instance is not None:
-            edit_form = forms.TournamentEditForm(instance=instance)
-            import_form = forms.TournamentImportForm(instance=instance)
-            context['import_form'] = import_form
-        else:
-            today = datetime.date.today()
-            edit_form = forms.TournamentEditForm(initial={'date': today})
+        edit_form = forms.TournamentEditForm(instance=instance)
+        import_form = forms.TournamentImportForm(instance=instance)
+        context['import_form'] = import_form
     context['edit_form'] = edit_form
     return render(request, 'tmdb/tournament_edit.html', context)
 
