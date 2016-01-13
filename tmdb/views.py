12a533fa799674ce702d5dcdf327913ad6a1d62f
Michelle Chen <michelle1919chen@gmail.com>
2016-01-12 23:20:36 -0500

    initial population of data on the dashboard. However, links are definitely not correct since they are not tournament specific.

--- tmdb/views.py
+++ tmdb/views.py
@@ -71,14 +71,16 @@ def tournament_import(request, tournament_slug):
     return HttpResponseRedirect(reverse('tmdb:index'))
 
 def tournament_dashboard(request, tournament_slug):
-    tournament = get_object_or_404(models.Tournament, slug=tournament_slug)
+    tournament = get_object_or_404(models.Tournament, slug=tournament_slug) 
     organizations = models.TournamentOrganization.objects.filter(
             tournament=tournament).order_by('organization__name')
     for org in organizations:
         org.import_form = forms.TournamentOrganizationImportForm(instance=org)
+    divisions = models.Division.objects.all()
     context = {
         'tournament': tournament,
         'organizations': organizations,
+        'divisions': divisions,
     }
     return render(request, 'tmdb/tournament_dashboard.html', context)
 
