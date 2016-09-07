ashish-b10/tournament_manager

commit e7dc3b00ae83ad226a8329c0b29541cf03177fbb

Fix rendering for empty bracket cells
Previously, the bars for the bracket were still being drawn for bracket
slots with no match.

tmdb/static/css/bracket.css
@@ -21,18 +21,12 @@
   float: top;
   margin: 0;
   position: relative;
-  border-bottom: 1px;
-  border-bottom-style: solid;
-  border-bottom-color: black;
 }
 
 .lower_team_bracket_cell {
   float: top;
   margin: 0;
   position: relative;
-  border-top: 1px;
-  border-top-style: solid;
-  border-top-color: black;
 }
 
 .upper_team_bracket_cell_text {
@@ -50,8 +44,20 @@
   background-color: #dddddd;
 }
 
-.upper_child_cell > .bottom_quarter_cell , .upper_child_cell > .lower_team_bracket_cell , .lower_child_cell > .top_quarter_cell , .lower_child_cell > .upper_team_bracket_cell {
+.upper_child_cell.bracket_cell_with_match > .bottom_quarter_cell , .upper_child_cell.bracket_cell_with_match > .lower_team_bracket_cell , .lower_child_cell.bracket_cell_with_match > .top_quarter_cell , .lower_child_cell.bracket_cell_with_match > .upper_team_bracket_cell {
   border-right: 2px;
   border-right-style: solid;
   border-right-color: black;
+}
+
+.bracket_cell_with_match > .upper_team_bracket_cell {
+  border-bottom: 1px;
+  border-bottom-style: solid;
+  border-bottom-color: black;
+}
+
+.bracket_cell_with_match > .lower_team_bracket_cell {
+  border-top: 1px;
+  border-top-style: solid;
+  border-top-color: black;
 }

tmdb/views.py
@@ -363,16 +363,20 @@ def bracket(request, tournament_slug, division_slug):
         bracket_columns.append(bracket_column)
         for round_slot in range(round_num_matches):
             key = (round_num, round_slot)
+            cell_type = []
             if key not in matches:
                 match = models.TeamMatch()
+                cell_type.append("bracket_cell_without_match")
             else:
                 match = matches[key]
-            match.height = str(100 / (round_num_matches)) + "%"
+                cell_type.append("bracket_cell_with_match")
             if round_slot % 2:
-                match.cell_type = "lower_child_cell"
+                cell_type.append("lower_child_cell")
             else:
-                match.cell_type = "upper_child_cell"
+                cell_type.append("upper_child_cell")
             bracket_column[round_slot] = match
+            match.cell_type = " ".join(cell_type)
+            match.height = str(100 / (round_num_matches)) + "%"
     if (0, 0) in matches:
         del matches[(0, 0)].cell_type # remove cell_type from final
     context = {

tmdb/templates/tmdb/brackets.html
@@ -8,7 +8,7 @@
   {% for bracket_column in bracket_columns %}
     <div class="bracket_column" style="height: {{bracket_column_height}}">
     {% for bracket_cell in bracket_column %}
-      <div class="bracket_cell {{ bracket_cell.with_match}} {{bracket_cell.cell_type}}" style="height: {{bracket_cell.height}}">
+      <div class="bracket_cell {{bracket_cell.cell_type}}" style="height: {{bracket_cell.height}}">
         <div class="bracket_cell_quarter top_quarter_cell"></div>
         <div class="bracket_cell_quarter upper_team_bracket_cell">
           <div class="upper_team_bracket_cell_text">{{bracket_cell.blue_team|default_if_none:""}}</div>

commit e42d2c2c44f47d0bf4dbac4322bd9d18b2e79e64

Fix rendering for finals in bracket
The line was not showing due to a regression from e7dc3b00

tmdb/static/css/bracket.css
@@ -50,7 +50,7 @@
   border-right-color: black;
 }
 
-.bracket_cell_with_match > .upper_team_bracket_cell {
+.bracket_cell_with_match > .upper_team_bracket_cell, .bracket_cell_with_match > .bracket_finals_cell {
   border-bottom: 1px;
   border-bottom-style: solid;
   border-bottom-color: black;

tmdb/views.py
@@ -378,7 +378,8 @@ def bracket(request, tournament_slug, division_slug):
             match.cell_type = " ".join(cell_type)
             match.height = str(100 / (round_num_matches)) + "%"
     if (0, 0) in matches:
-        del matches[(0, 0)].cell_type # remove cell_type from final
+        matches[(0, 0)].cell_type = "bracket_cell_with_match" \
+                + " bracket_finals_cell"
     context = {
             'tournament_division': tournament_division,
             'tournament': tournament,