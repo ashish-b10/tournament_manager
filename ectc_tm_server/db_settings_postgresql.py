82f213e1461eef44e43481d3abb7238ed658d811
Ashish Banerjee <profcalc14@yahoo.com>
2016-09-06 22:58:02 -0400

    Create multiple settings files for database
    Create a separate settings file for SQLite and PostgreSQL. The actual
    one must be symlinked by the user

---
+++ ectc_tm_server/db_settings_postgresql.py
@@ -0,0 +1,9 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tmdb',
+        'USER': 'tmdb',
+        'HOST': 'localhost',
+    }
+}
+
