5a99e7aa6c038979c247b7a4602bdf9787ce3951
Michelle Chen <michelle1919chen@gmail.com>
2017-03-26 14:31:17 -0400

    update to format it for treant-style.

--- tmdb/util/d3_bracket_layout.py
+++ tmdb/util/d3_bracket_layout.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 __all__ = ['d3_bracket_layout']
 
 def d3_bracket_layout(tournament_division, matches_by_round):
@@ -6,7 +8,8 @@ def d3_bracket_layout(tournament_division, matches_by_round):
     if (0,0,) not in matches_by_round:
         return None
     root_match = matches_by_round[(0,0,)]
-    return __get_d3_bracket_layout(root_match, matches_by_round)
+    max_round_slot = max((m[0] for m in matches_by_round.keys()))
+    return __get_d3_bracket_layout(root_match, matches_by_round, max_round_slot)
 
 def __get_upper_competitor(match, matches_by_round):
     return matches_by_round.get((
@@ -16,44 +19,48 @@ def __get_lower_competitor(match, matches_by_round):
     return matches_by_round.get((
             match.round_num + 1, match.round_slot * 2 + 1,))
 
-def __get_d3_round_name(match):
+def __get_d3_bracket_layout(match, matches_by_round, max_round_slot):
+    layout = {}
+    layout['text'] = OrderedDict()
+    layout['text']['name'] = "" 
     if match.winning_team:
-        return match.winning_team.team.short_str()
-    return str(match)
+        layout['text']['name'] = match.winning_team.short_str()
 
-def __get_d3_bracket_layout(match, matches_by_round):
-    layout = {
-        "text": {
-            "name": __get_d3_round_name(match)
-        }
-    }
-
+    layout['text']['desc'] = str(match)
     children = []
 
     upper_child = __get_upper_competitor(match, matches_by_round)
     if upper_child:
-        children.append(__get_d3_bracket_layout(upper_child, matches_by_round))
+        children.append(__get_d3_bracket_layout(upper_child, matches_by_round, max_round_slot))
     else:
         new_child = {
-            "text": {
-                "name": "Bye"
-            }
+            "text": OrderedDict()
         }
         if match.blue_team:
             new_child['text']['name'] = match.blue_team.team.short_str()
+            if match.round_num == max_round_slot:
+                new_child['HTMLclass'] = "first-draw"
+            else:
+                new_child['text']['desc'] = ""
+        else:
+            new_child['text']['name'] = 'Bye'
         children.append(new_child)
 
     lower_child = __get_lower_competitor(match, matches_by_round)
     if lower_child:
-        children.append(__get_d3_bracket_layout(lower_child, matches_by_round))
+        children.append(__get_d3_bracket_layout(lower_child, matches_by_round, max_round_slot))
     else:
         new_child = {
-            "text": {
-                "name": "Bye"
-            }
+            "text": OrderedDict()
         }
         if match.red_team:
             new_child['text']['name'] = match.red_team.team.short_str()
+            if match.round_num == max_round_slot:
+                new_child['HTMLclass'] = "first-draw"
+            else:
+                new_child['text']['desc'] = ""
+        else:
+            new_child['text']['name'] = 'Bye'
         children.append(new_child)
     layout['children'] = children
 
