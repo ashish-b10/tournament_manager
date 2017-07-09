b1250e753411871d190c3b34d30ddf077a2ae268
Ashish Banerjee <profcalc14@yahoo.com>
2017-07-09 10:31:40 -0400

    Send entire database to webclient on load
    This commit sends all of the information necessary for rendering the
    match list (and probably a lot more than is necessary). The client
    stores it all in a hierarchy of objects as reference for rendering the
    display.

--- tmdb/consumers.py
+++ tmdb/consumers.py
@@ -1,11 +1,58 @@
+import json
+
+from django.core import serializers
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from channels import Group
 from channels.sessions import channel_session
+
 from . import models
 
 def match_updates_group_name(tournament_slug):
     return "match-updates-%s" %(tournament_slug,)
+
+def model_to_json(query_set, fields):
+    obj_json = serializers.serialize('json', query_set, fields=fields)
+    return json.loads(obj_json)
+
+def initial_tournament_data(tournament_slug):
+    msg = []
+    tournament = models.Tournament.objects.get(slug=tournament_slug)
+    msg.extend(model_to_json(
+            [tournament],
+            ('id', 'location', 'date',)))
+    msg.extend(model_to_json(
+            models.Division.objects.all(),
+            ('id', 'sex', 'skill_level',)))
+    msg.extend(model_to_json(
+            models.School.objects.all(),
+            ('id', 'name',)))
+    msg.extend(model_to_json(
+            models.Team.objects.all(),
+            ('id', 'division', 'school', 'number',)))
+    msg.extend(model_to_json(
+            models.TournamentDivision.objects.filter(tournament=tournament),
+            ('id', 'division', 'tournament',)))
+    msg.extend(model_to_json(
+            models.SchoolRegistration.objects.filter(tournament=tournament),
+            ('id', 'school', 'tournament',)))
+    msg.extend(model_to_json(
+            models.Competitor.objects.filter(
+                    registration__tournament=tournament),
+            ('id', 'registration', 'belt_rank', 'name', 'sex',)))
+    msg.extend(model_to_json(
+            models.TeamRegistration.objects.filter(
+                    tournament_division__tournament=tournament),
+            ('id', 'lightweight', 'middleweight', 'heavyweight', 'alternate1',
+                    'alternate2', 'team', 'tournament_division', 'points',
+                    'seed',)))
+    msg.extend(model_to_json(
+            models.TeamMatch.objects.filter(division__tournament=tournament),
+            ('id', 'blue_team', 'red_team', 'winning_team', 'division',
+                    'in_holding', 'number', 'ring_assignment_time',
+                    'ring_number', 'round_num', 'round_slot',)))
+
+    return msg
 
 @receiver(post_save, sender=models.TeamMatch,
         dispatch_uid="update_team_matches")
@@ -19,6 +66,8 @@ def update_team_matches(sender, instance, **kwargs):
 def match_updates_connect(message, tournament_slug):
     message.channel_session['tournament_slug'] = tournament_slug
     message.reply_channel.send({"accept": True})
+    message.reply_channel.send(
+            {"text": json.dumps(initial_tournament_data(tournament_slug))})
     group_name = match_updates_group_name(tournament_slug)
     Group(group_name).add(message.reply_channel)
 
--- tmdb/static/js/match_websocket.js
+++ tmdb/static/js/match_websocket.js
@@ -1,4 +1,5 @@
 tmdb_vars = {};
+tmdb_vars.tournament_data = {};
 
 function start_teammatch_websocket(tournament_slug) {
   if (tmdb_vars.match_update_ws == null) {
@@ -6,9 +7,22 @@ function start_teammatch_websocket(tournament_slug) {
     tmdb_vars.match_update_ws = new WebSocket(ws_url);
     tmdb_vars.match_update_ws.onmessage = function(msg) {
       console.log(msg);
+      var msg_data = JSON.parse(msg.data);
+      var tournament_data = tmdb_vars.tournament_data.tournament_slug;
+      msg_data.map(function(d) {
+        if (d.model == "tmdb.tournament") {
+          tournament_data[d.model] = d;
+          return;
+        }
+        if (tournament_data[d.model] == undefined) {
+          tournament_data[d.model] = {};
+        }
+        tournament_data[d.model][d.pk] = d;
+      });
     }
     tmdb_vars.match_update_ws.onopen = function() {
       console.log("Opened connection to " + ws_url);
+      tmdb_vars.tournament_data.tournament_slug = {}
     }
     tmdb_vars.match_update_ws.onclose = function() {
       setInterval(function() {
