a4a276df009ac5cb48d0b24ca8f3ccbd5f3154b8
Michelle Chen <michelle1919chen@gmail.com>
2018-01-20 10:31:21 -0500

    initial start to making the left navbar collapse. Need to clean up styling still.

---
+++ tmdb/static/js/tournament_dashboard.js
@@ -0,0 +1,7 @@
+(function($) {
+  "use strict"; // Start of use strict
+  $("#sidenavToggler").click(function(e) {
+    $("#mainNav").toggleClass("active");
+  });
+
+})(jQuery); // End of use strict
