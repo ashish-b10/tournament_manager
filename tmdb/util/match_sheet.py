99458e863a6071a3cb293f3540f788d0ff2de34c
Ashish Banerjee <profcalc14@yahoo.com>
2018-10-08 17:17:04 -0400

    Update to use new match sheet

--- tmdb/util/match_sheet.py
+++ tmdb/util/match_sheet.py
@@ -23,47 +23,48 @@ def _get_match_sheet_template_fn():
     this_directory = os.path.dirname(__file__)
     return os.path.join(this_directory, 'match_sheet.pdf')
 
+def _draw_ellipse(drawing_canvas, xoffset, yoffset):
+    drawing_canvas.ellipse(
+            3.2*inch + xoffset, 9.5*inch + yoffset,
+            2.4*inch + xoffset, 9.1*inch + yoffset)
+
 def _draw_match_sheet(match, female=False, a_team=False, b_team=False, c_team=False):
     drawing_layer = StringIO()
     drawing_canvas = canvas.Canvas(drawing_layer, pagesize=letter)
     drawing_canvas.setLineWidth(3)
-    xoffset, yoffset = 0, 0
+    xoffset, yoffset = 0, .1*inch
     if match.division.division.sex == models.SexField.MALE:
-        drawing_canvas.ellipse(
-                3.2*inch + xoffset, 9.5*inch + yoffset,
-                2.4*inch + xoffset, 9.1*inch + yoffset)
+        _draw_ellipse(drawing_canvas, xoffset, yoffset)
     xoffset = 4.23 * inch
     if match.division.division.sex == models.SexField.FEMALE:
-        drawing_canvas.ellipse(
-                3.2*inch + xoffset, 9.5*inch + yoffset,
-                2.4*inch + xoffset, 9.1*inch + yoffset)
-    yoffset = -.3 * inch
+        _draw_ellipse(drawing_canvas, xoffset, yoffset)
+    yoffset = -.23 * inch
     skill_level = match.division.division.skill_level
     if skill_level == models.DivisionLevelField.C_TEAM_VAL:
-        drawing_canvas.ellipse(
-                3.2*inch + xoffset, 9.5*inch + yoffset,
-                2.4*inch + xoffset, 9.1*inch + yoffset)
+        _draw_ellipse(drawing_canvas, xoffset, yoffset)
     xoffset = 0
     if skill_level == models.DivisionLevelField.A_TEAM_VAL:
-        drawing_canvas.ellipse(
-                3.2*inch + xoffset, 9.5*inch + yoffset,
-                2.4*inch + xoffset, 9.1*inch + yoffset)
+        _draw_ellipse(drawing_canvas, xoffset, yoffset)
     xoffset = 4.23/ 2 * inch
     if skill_level == models.DivisionLevelField.B_TEAM_VAL:
-        drawing_canvas.ellipse(
-                3.2*inch + xoffset, 9.5*inch + yoffset,
-                2.4*inch + xoffset, 9.1*inch + yoffset)
+        _draw_ellipse(drawing_canvas, xoffset, yoffset)
     drawing_canvas.setFont('Helvetica', 24)
-    drawing_canvas.drawString(1.7*inch, 7.8*inch, str(match.number))
-    drawing_canvas.drawString(1.0*inch, 10.7*inch,
+    xoffset = 1.8 * inch
+    yoffset = 8.23 * inch
+    drawing_canvas.drawString(xoffset, yoffset, str(match.number))
+    if match.ring_number:
+        drawing_canvas.drawString(xoffset + 3.65 * inch, yoffset,
+            str(match.ring_number))
+    drawing_canvas.drawString(6.3*inch, 10.4*inch,
             _get_round_str(match.round_num))
     drawing_canvas.setFont('Helvetica', 18)
+    xoffset = .65 * inch
+    yoffset = 7.25 * inch
     if match.blue_team:
-        drawing_canvas.drawString(.65*inch, 6.65*inch,
+        drawing_canvas.drawString(xoffset, yoffset,
                 str(match.blue_team))
-    xoffset = 3.75*inch
     if match.red_team:
-        drawing_canvas.drawString(.65*inch + xoffset, 6.65*inch,
+        drawing_canvas.drawString(xoffset + 3.75*inch, yoffset,
                 str(match.red_team))
 
     return PdfFileReader(BytesIO(drawing_canvas.getpdfdata())).getPage(0)
