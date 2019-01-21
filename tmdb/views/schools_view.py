1a3f021b0d9baf677edaead1b9c881df8dc00f04
Ashish Banerjee <profcalc14@yahoo.com>
2019-01-21 11:52:53 -0500

    Implement UI to change SchoolSeasonRegistration
    The primary necessity of this feature is the ability to edit which
    division a school is in for a particular season.

--- tmdb/views/schools_view.py
+++ tmdb/views/schools_view.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import permission_required
+from django import forms
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 
 from tmdb import models
 
@@ -20,9 +23,40 @@ def school(request, school_slug):
     }
     return render(request, 'tmdb/school.html', context)
 
+class SchoolSeasonRegistrationAddChangeForm(forms.ModelForm):
+    class Meta:
+        model = models.SchoolSeasonRegistration
+        exclude = ['school', 'season']
+
+    def __init__(self, *args, **kwargs):
+        super(SchoolSeasonRegistrationAddChangeForm, self).__init__(
+                *args, **kwargs)
+        division_widget = forms.Select(
+                choices=((1,"Div #1"), (2,"Div #2"),(3, "Div #3")))
+        if self.instance:
+            division_widget.initial = self.instance.division
+        else:
+            division_widget.initial = 3
+        self.fields['division'].widget = division_widget
+
 @permission_required("tmdb.change_schoolseasonregistration")
 def school_season_change(request, school_slug, season_slug):
-    raise NotImplementedError()
+    instance = get_object_or_404(models.SchoolSeasonRegistration,
+            school__slug=school_slug, season__slug=season_slug)
+    template_name = 'tmdb/school_season_registration_change.html'
+    context = {}
+    if request.method == 'POST':
+        change_form = SchoolSeasonRegistrationAddChangeForm(request.POST,
+                instance=instance)
+        if change_form.is_valid():
+            change_form.save()
+            return HttpResponseRedirect(reverse('tmdb:school',
+                    args=(school_slug,)))
+    else:
+        change_form = SchoolSeasonRegistrationAddChangeForm(
+                instance=instance)
+    context['change_form'] = change_form
+    return render(request, template_name, context)
 
 @permission_required("tmdb.change_school")
 def school_change(request, school_slug):
