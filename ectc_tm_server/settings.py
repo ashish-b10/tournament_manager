96b6b57d6fb02e1d4c6cd381fc2b784e99faf903
Ashish Banerjee <profcalc14@yahoo.com>
2019-10-01 19:23:10 -0400

    Move DATABASES and CACHES to db_settings.py

--- ectc_tm_server/settings.py
+++ ectc_tm_server/settings.py
@@ -89,39 +89,7 @@ WSGI_APPLICATION = 'ectc_tm_server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-if 'DB_NAME' in os.environ:
-    # Running the Docker image
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['DB_NAME'],
-            'USER': os.environ['DB_USER'],
-            'PASSWORD': os.environ['DB_PASS'],
-            'HOST': os.environ['DB_SERVICE'],
-            'PORT': os.environ['DB_PORT']
-        }
-    }
-else:
-    # Building the Docker image
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://redis:6379/0",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    }
-}
-
-SESSION_ENGINE = "django.contrib.sessions.backends.cache"
-SESSION_CACHE_ALIAS = "default"
+from .db_settings import *
 
 AUTH_PASSWORD_VALIDATORS = [
     {
