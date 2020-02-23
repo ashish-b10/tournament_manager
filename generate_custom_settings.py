775d0cdfdc68bf851b7676a83b872a29938cddc5
Ashish Banerjee <github@ashishbanerjee.net>
2020-02-23 11:24:45 -0500

    Move generate_custom_settings.py up one directory
    This puts it in the same directory as the runserver.py command, so users
    will not have to change directory or paths.

--- generate_custom_settings.py
+++ generate_custom_settings.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+from pathlib import Path
+
 from django.core.management.utils import get_random_secret_key
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -8,7 +10,8 @@ SECRET_KEY = get_random_secret_key()
 DEBUG = True
 
 # Write out the python file
-with open("custom_settings.py","w") as fh:
+output_file = Path(__file__).parent / 'ectc_tm_server' / 'custom_settings.py'
+with open(output_file, "w") as fh:
     fh.write("SECRET_KEY=\"" + SECRET_KEY +"\"\n")
     fh.write("DEBUG=" + str(DEBUG) + "\n")
     fh.write("ALLOWED_HOSTS = [\"localhost\"]\n")
